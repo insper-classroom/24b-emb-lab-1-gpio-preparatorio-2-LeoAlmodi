4942c46e63614bdc0ebc970b738ffbabae68c589
LeoAlmodi <leonardoar2@al.insper.edu.br>
2024-08-15 21:44:10 -0300

    Ex3-done

--- exe3/main.c
+++ exe3/main.c
@@ -2,9 +2,40 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int PIN_R = 4;
+const int PIN_G = 6;
+const int BTN_PIN_R = 28;
+const int BTN_PIN_G = 26;
+
 int main() {
   stdio_init_all();
 
+  gpio_init(PIN_R);
+  gpio_set_dir(PIN_R, GPIO_OUT);
+
+  gpio_init(PIN_G);
+  gpio_set_dir(PIN_G, GPIO_OUT);
+
+  gpio_init(BTN_PIN_R);
+  gpio_set_dir(BTN_PIN_R, GPIO_IN);
+  gpio_pull_up(BTN_PIN_R);
+
+  gpio_init(BTN_PIN_G);
+  gpio_set_dir(BTN_PIN_G, GPIO_IN);
+  gpio_pull_up(BTN_PIN_G);
+
   while (true) {
+    if(!gpio_get(BTN_PIN_G)){
+      int status = !gpio_get(PIN_G);
+      gpio_put(PIN_G, status);
+      while (!gpio_get(BTN_PIN_G)){
+      }
+    }
+    if(!gpio_get(BTN_PIN_R)){
+      int status = !gpio_get(PIN_R);
+      gpio_put(PIN_R, status);
+      while (!gpio_get(BTN_PIN_R)){
+      }
+    }
   }
 }
